9572f086b85c5a88d2ee8cf6ab355759e5e871c7
Fulvio <fulviocanducci@hotmail.com>
2019-08-12 12:34:43 -0300

    new first one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect  } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    document.title = list.length;
+  }, [count, list]);
+
+  function handleKeyPress (event) {    
+    if(event.key === 'Enter') {
+      setList([...list, {name: name}]);
+      setName('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {count} <button onClick={() => setCount(count + 1)}>Adicionar</button>
+      </div>
+      <div>
+        {
+          list.map((item,i) => (
+            <li key={i}>{item.name}</li>
+          ))
+        }
+      </div>
+      <div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} onKeyPress={handleKeyPress} /> 
+        <button onClick={() => setList([...list, {name: name}])}>Add</button>
+      </div>
+    </>
   );
 }
 
